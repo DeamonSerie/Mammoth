8273d22f0f42fce2131d142b70ff3066a22e24db
Mammoth Developer <developer@mammoth.local>
2026-08-19 14:05:09 -0500

    MoveTool: revert begin() save-if-empty change, always save layer data
    - begin() now always overwrites m_savedData with current layer state
    - Combined with the end() fix (no clear), this should preserve content across moves
    - Will test to see if this resolves the 'skips to original position' issue

--- native/src/drawing/MoveTool.cpp
+++ native/src/drawing/MoveTool.cpp
@@ -16,7 +16,7 @@ void MoveTool::begin(Layer& layer, Canvas& canvas, const Rect& canvasRect,
     m_prevOffset.y = 0;
     m_layerW = layer.width();
     m_layerH = layer.height();
-    if (m_savedData.empty()) m_savedData.assign(layer.data(), layer.data() + layer.dataSize());
+    m_savedData.assign(layer.data(), layer.data() + layer.dataSize());
 }
 
 void MoveTool::update(Layer& layer, Canvas& canvas, const Rect& canvasRect,
